9e44461f4b6c041b4b2684a5cde4f71c68089bdd
Wizcom12 <118827515+Wizcom12@users.noreply.github.com>
2023-02-01 23:09:19 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+
+
+ button = document.getElementById("survey-button-1");
+ button.addEventListener("click", function() {
+   window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScQzD9YtWrHhZddE6oQKlvGAdM9jCV4voS3mPgKj-0V2hCWFA/viewform";
+ });
+ 
+
+ button = document.getElementById("survey-button-2");
+button.addEventListener("click", function() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeLMgVBbM6ikIcreszFQU2GicUbAHYBNo6iDy-UZDxCwSviZQ/viewform";
+});
+
+
+
+
+button = document.getElementById("survey-button-3");
+button.addEventListener("click", function() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSf9gZDiEXJ8FfMUtIbBUtR3ZnYM84KoiZymbvPH3xmT8CjKvQ/viewform";
+});
+
+
+
+button = document.getElementById("survey-button-4");
+button.addEventListener("click", function() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSebAzZLBFCwuUXaPYqsvxO9SA-5CXpX2qBQvTZtL5bnEIC4tg/viewform";
+});
+
+
+
+
+button = document.getElementById("survey-button-5");
+button.addEventListener("click", function() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSc9Vm6yRfWcOfnM7yfOdwMkjxzvVCN81IAx6QppqBNRhkrknQ/viewform";
+});
+
+
+/*navigation bar */
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
