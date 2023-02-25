4d68862792a6b7746536908efa52fbce22407919
Wizcom12 <118827515+Wizcom12@users.noreply.github.com>
2023-02-25 08:23:49 -0800

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("count").innerHTML = "EXPIRED";
+    document.getElementById("count").innerHTML = "VOTING ENDED";
   }
 }, 1000);
 
